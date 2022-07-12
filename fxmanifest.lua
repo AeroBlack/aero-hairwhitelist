8be7835cfb49e62cd57b426ef5c4f06e9e43708b
AeroBlack <109138237+AeroBlack@users.noreply.github.com>
2022-07-12 16:20:46 +0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+description 'Aero HairWL'
+
+version '0.1.0'
+
+shared_script '@es_extended/imports.lua'
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
+
+dependencies {
+	'es_extended',
+	'skinchanger'
+}
